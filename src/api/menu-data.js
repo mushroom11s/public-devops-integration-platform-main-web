d6910dcde341ac735bfee1fa53fb818534fc9c61
aries zhou <aries.zhou@awcs.com>
2022-02-08 11:38:37 +0800

    Project init

--- src/api/menu-data.js
+++ src/api/menu-data.js
@@ -2,7 +2,7 @@ import request from '@/utils/request'
 // menu detail
 export function getMenuDetails() {
   return request({
-    url: 'http://localhost:8022/menu.json',
+    url: 'http://localhost:8080/menu.json',
     method: 'get',
   })
 }
